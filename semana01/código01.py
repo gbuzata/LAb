65bdc7ff324b5caadf7e25ac2612e06f9c7265e3
gbuzata <141975611+gbuzata@users.noreply.github.com>
2023-08-17 20:30:48 -0300

    Create código01.py

---
+++ semana01/código01.py
@@ -0,0 +1,19 @@
+idade = 20
+nome = "Guilherme Buzata Araujo"
+CPF = "03687010007"
+peso = 112.6
+dataDeNascimento = "07/05/2003"
+cidade = "Jaguari"
+estado = "RS"
+velocidade = 80
+preco = 29.90
+
+print("Minha idade:",(type),idade)
+print("Meu nome é:", nome)
+print("CPF:", CPF)
+print("Meu peso:", peso)
+print("Nasci no dia:",dataDeNascimento )
+print("Minha cidade é:", cidade)
+print("Estado:", estado)
+print("Velocidade:", velocidade)
+print("Valor da refeição:", preco)
